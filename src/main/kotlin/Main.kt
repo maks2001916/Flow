3ee7b5c284cac464ac90254c0384553fec0a4772
maks2001916 <mamonov.maksim.20003@gmail.com>
2024-11-16 17:51:00 +0500

    т.з. выполнено

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,55 +13,65 @@ import kotlin.system.measureTimeMillis
 suspend fun main() {
     val size = persons.size
     val time = measureTimeMillis {
-        withContext(newSingleThreadContext("My_Thread_Context")) {
-            var personVar: Person? = null
-            launch {
-                getPersonsFlow().collect { i ->
-                    //print(i)
-                    personsTwo.add(i)
+        newSingleThreadContext("My_Thread_Context").use { threadContext ->
+            withContext(threadContext) {
+                val personsJob = launch {
+                    getPersonsFlow().collect { i ->
+                        personsTwo.add(i)
+                    }
                 }
-            }
-            launch {
-                getPhoneFlow(size).collect { i ->
-                    delay(100L)
-                    phones.add(i)
+                val phonesJob = launch {
+                    getPhoneFlow(size).collect { i ->
+                        delay(100L)
+                        phones.add(i)
+                    }
                 }
-            }
-            launch {
-                print("[")
-                delay(150L*size)
-                personsTwo.forEachIndexed { index, person ->
-                    personInfo.set(person, phones.get(index))
-                }
-                personsTwo.asFlow().onEach { print(it) }
-                print("]")
 
+                // Ждем завершения всех корутин
+                personsJob.join()
+                phonesJob.join()
+
+                // Теперь можно производить вывод
+                val printJob = launch {
+                    print("[")
+                    personsTwo.forEachIndexed { index, person ->
+                        personInfo[person] = phones[index]
+                        print("${person} ${phones[index]}, ")
+                    }
+                    print("]")
+                }
+
+                // Ждем завершения вывода
+                printJob.join()
             }
         }
     }
 
-    time
-
-
+    println("\nExecution time: $time ms")
 }
+
 val persons = listOf(
     Person("Игорь", "Разработчик"),
-    Person("Максим", "врач"),
-    Person("Анастасия", "юрист"),
-    Person("Виктория", "учитель"))
+    Person("Максим", "Врач"),
+    Person("Анастасия", "Юрист"),
+    Person("Виктория", "Учитель")
+)
+
 fun getPersonsFlow() = persons.asFlow().onEach { delay(100L) }
 
 fun getPhoneFlow(length: Int) = flow {
-    for (i in 1 .. length) {
-        val random = Random(System.currentTimeMillis())
-        val stringBuilder = StringBuilder(length)
-        repeat(length) {
+    for (i in 1..length) {
+        delay(100L) // Simulating some delay for fetching phone numbers
+        val random = Random(System.currentTimeMillis() + i) // Different seed for each number
+        val stringBuilder = StringBuilder()
+        repeat(10) {
             val randomDigit = random.nextInt(0, 10)
             stringBuilder.append(randomDigit)
         }
-        emit("+7914${stringBuilder}")
+        emit("+7${stringBuilder}") // Измените номер телефона, чтобы он был к правильному формату
     }
 }
+
 val personsTwo = mutableListOf<Person>()
 val phones = mutableListOf<String>()
 val personInfo = mutableMapOf<Person, String>()
